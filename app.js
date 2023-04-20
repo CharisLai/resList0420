b68412b226760a6e705f0224f736de3614a9c1b6
CharisLai <griffin8418@gmail.com>
2023-04-20 20:52:59 +0800

    feat: Show.page with handlebars

--- app.js
+++ app.js
@@ -11,9 +11,16 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 
-// routes
+// routes: index
 app.get('/', (req, res) => {
     res.render('index', { data: dataList.results })
+})
+
+// routes: show
+app.get('/restaurants/:id', (req, res) => {
+    console.log('req.params.id', req.params.id)
+    const One = dataList.results.find(One => One.id.toString() === req.params.id)
+    res.render('show', { show: One })
 })
 
 app.listen(3000, () => {
