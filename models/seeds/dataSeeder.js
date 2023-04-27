5e0c89b4aa2d7b5d7d354540f691ed4e20f21f38
CharisLai <griffin8418@gmail.com>
2023-04-28 00:03:20 +0800

    modify dataSeeder

--- models/seeds/dataSeeder.js
+++ models/seeds/dataSeeder.js
@@ -1,15 +1,51 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 const db = require('../../config/mongoose')
 // loading model and seeder.json
-const Restaurant = require('../restaurant')
+const User = require('../user')
+const restaurant = require('../restaurant')
 const dataList = require('../restaurant.json').results
-
+const SEED_USER = [{
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678',
+    indexField: [0, 1, 2]
+},
+{
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678',
+    indexField: [3, 4, 5]
+}
+]
 // mongodb connection status
 db.once('open', () => {
-    console.log('MongoDB Working!')
-    Restaurant.create(dataList)
+    Promise
+        .all(SEED_USER.map(user => {
+            const { name, email, password, indexField } = user
+            return User.create(
+                {
+                    name,
+                    email,
+                    password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+                }
+            )
+                .then(user => {
+                    const restaurants = indexField.map(index => {
+                        const restaurant = dataList[index]
+                        restaurant.userId = user._id
+                        return restaurant
+                    })
+                    console.log('MongoDB Working!')
+                    return restaurant.create(restaurants)
+                })
+        }))
         .then(() => {
             console.log('DataSeeder is DONE.')
-            db.close()
+            process.exit()
         })
-        .catch(err => console.error)
+        .catch(error => console.error(error))
+        .finally(() => db.close)
 })
